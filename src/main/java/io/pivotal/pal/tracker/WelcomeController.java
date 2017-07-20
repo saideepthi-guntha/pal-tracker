9d7c1147b64415a8efb27e763b1613a5c1b816e3
Tyson Gern <tgern@pivotal.io>
2017-07-20 13:56:50 -0600

    Adding Controllers

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,5 +1,6 @@
 package io.pivotal.pal.tracker;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,17 +10,26 @@ public class WelcomeController {
     @Autowired
     private Service service;
 
+    private String message;
 
     @RequestMapping("/")
     public String sayHello(){
-        return "hello";
+        return message;
     }
     @RequestMapping("/Bye")
     public String sayBye(){
       return service.sayBye();
 
+    }
+
+
+
+    public WelcomeController(@Value("${WELCOME_MESSAGE}") String message){
+        this.message=message;
 
     }
+
+
 
 
 }
